e6f7498c2a612e0f4ec00cea93eb229a19766ff3
Jake Moretz <jmoretz9090@gmail.com>
2023-05-08 22:39:19 -0400

    added webpack files

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    dom: './src/dom.js',
+    handler: './src/handlers.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
