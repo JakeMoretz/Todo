9e4a15a3b7d0eba0573a36aa1084ecd3c638fc60
Jake Moretz <jmoretz9090@gmail.com>
2023-05-10 23:05:00 -0400

    working on added cards to main todo

--- src/handlers.js
+++ src/handlers.js
@@ -1,17 +1,54 @@
-// add project card/folder
+// add/delete project card/folder
 const newProjectButton = document.querySelector('.new-project-btn');
 const projects = document.querySelector('.projects');
 const projectName = document.querySelector('input');
 
+
 newProjectButton.addEventListener('click', () => {
+    if (!projectName.value) {
+        return;
+    }
+
     const projectDiv = document.createElement('div');
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.className = 'deleteBtn';
     projectDiv.className = 'project-card';
+
     projectDiv.textContent = projectName.value;
     projects.appendChild(projectDiv);
+    projectDiv.appendChild(deleteBtn);
+
+
+    deleteBtn.addEventListener("click", () => {
+        projectDiv.remove();
+    })
+   
+
+
+   
 
     clearInput();
+
+   
+
+    
 });
 
 function clearInput() {
     projectName.value = '';
 }
+
+
+    // projectDiv.value.addEventListener('click', () => {
+    //     const toDoSection = document.querySelector(".main");
+    //     const toDo = document.createElement("div");
+    //     toDo.className = "test";
+    //     toDoSection.appendChild(toDo);
+
+    // });
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import "./index.css";
 import "./handlers";
-import "./dom";
+
 
