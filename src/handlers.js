43177455fdd465500da3a7edc34ceaedf8e8f065
Jake Moretz <jmoretz9090@gmail.com>
2023-05-12 22:51:33 -0400

    added event listener to add card

--- src/handlers.js
+++ src/handlers.js
@@ -1,54 +1,11 @@
-// add/delete project card/folder
-const newProjectButton = document.querySelector('.new-project-btn');
-const projects = document.querySelector('.projects');
-const projectName = document.querySelector('input');
+import './index.js';
+import { addToDo } from './index.js';
 
-
-newProjectButton.addEventListener('click', () => {
-    if (!projectName.value) {
-        return;
-    }
-
-    const projectDiv = document.createElement('div');
-    const deleteBtn = document.createElement('button');
-
-    deleteBtn.textContent = 'Delete';
-    deleteBtn.className = 'deleteBtn';
-    projectDiv.className = 'project-card';
-
-    projectDiv.textContent = projectName.value;
-    projects.appendChild(projectDiv);
-    projectDiv.appendChild(deleteBtn);
-
-
-    deleteBtn.addEventListener("click", () => {
-        projectDiv.remove();
-    })
-   
-
-
-   
-
-    clearInput();
-
-   
-
-    
-});
-
-function clearInput() {
-    projectName.value = '';
-}
-
-
-    // projectDiv.value.addEventListener('click', () => {
-    //     const toDoSection = document.querySelector(".main");
-    //     const toDo = document.createElement("div");
-    //     toDo.className = "test";
-    //     toDoSection.appendChild(toDo);
-
-    // });
-
-
-
-
+// event listener for adding a todo
+export const addButtonEventListener = () => {
+    const addButton = document.querySelector('.new-project-btn');
+    addButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        addToDo();
+    });
+};
